f34efd17cfc3da561a7d5e4e9335982bb640a242
motty-mio2 <motty.mio2@gmail.com>
2021-06-01 13:26:31 +0900

    release version for windows

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,13 @@
-Get-ChildItem -Path (Join-Path (Split-Path $profile -Parent) "\conf\") -Name -Recurse -Include *.ps1 | ForEach-Object -Process {
-    Invoke-Expression (". " + (Join-Path (Split-Path $profile -Parent) "\conf\") + $_)
+$configure_folder = Split-Path $profile -Parent
+
+Get-ChildItem -Path (Join-Path $configure_folder "\conf\") -Name -Recurse -Include *.ps1 | ForEach-Object -Process {
+    Invoke-Expression (". " + (Join-Path $configure_folder "\conf\") + $_)
 }
 
-if (Test-Path (Join-Path (Split-Path $profile -Parent) "\addon\")) {
-    Get-ChildItem -Path (Join-Path (Split-Path $profile -Parent) "\addon\") -Name -Recurse -Include *.ps1 | ForEach-Object -Process {
-        Invoke-Expression (". " + (Join-Path (Split-Path $profile -Parent) "\addon\") + $_)
+
+$addon_folder = (Join-Path $configure_folder "\addon\")
+if (Test-Path $addon_folder) {
+    Get-ChildItem -Path $addon_folder -Name -Recurse -Include *.ps1 | ForEach-Object -Process {
+        Invoke-Expression (". " + $addon_folder + $_)
     }
 }
