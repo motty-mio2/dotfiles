474cf02da4b22c0f61166d3999ae71daa99b4e88
motty.mio2 <motty.mio2@gmail.com>
2021-06-02 16:59:27 +0900

    update addon script

--- conf/shell.sh
+++ conf/shell.sh
@@ -1,3 +1,5 @@
+addon_folder="${HOME}/conf/addon"
+
 # set PATH so it includes user's private bin if it exists
 if [ -d "$HOME/bin" ] ; then
     PATH="$HOME/bin:$PATH"
@@ -16,5 +18,8 @@ alias python='python3'
 
 eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
-(source ~/conf/addon/*.sh) &> /dev/null
-
+if [ -d $addon_folder ]; then
+    for file in $( ls $addon_folder | grep .sh$ ); do
+        source "${addon_folder}/${file}"
+    done
+fi
--- conf/zsh.sh
+++ conf/zsh.sh
@@ -1,5 +1,5 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-source ${SCRIPT_DIR}/shell.sh
+source "${SCRIPT_DIR}/shell.sh"
 
 plugins=(git zsh-autosuggestions)
 
