22bdc17bd688f612b91eb055214c80a9649a6e44
motty-mio2 <motty.mio2@gmail.com>
2021-06-04 18:09:25 +0900

    update prompt for git status

--- conf/prompt.ps1
+++ conf/prompt.ps1
@@ -1,15 +1,29 @@
 Import-Module posh-git
 Import-Module oh-my-posh
 Import-Module PSReadLine
+
 function global:prompt {
     $user = $env:UserName
     # $now = Get-Date -format "MM/dd HH:mm:ss"
     $now = Get-Date -format "HH:mm:ss"
-    $loc = Get-Location
+    $loc = (Get-Location).ToString().Replace($HOME, "~")
 
-    Write-Host("[" + $user + "] ") -nonewline -foregroundcolor DarkGreen
-    Write-Host("(" + $now + ")") -nonewline -foregroundcolor DarkYellow
-    Write-Host(" " + $loc + " ") -nonewline -foregroundcolor DarkBlue
+    # Write-Host("[" + $user + "] ") -nonewline -foregroundcolor DarkGreen
+    Write-Host("(" + $now + ") ") -nonewline -foregroundcolor DarkYellow
+    Write-Host($loc + " ") -nonewline -foregroundcolor DarkBlue
+
+    $branch = ((git branch --contains) 2>&1).toString()
+    if ($branch -notcontains "fatal: not a git repository") {
+        $name = $branch.split(" ")[1]
+        $status = git status
+        if ($status.Contains("Changes not staged for commit:")) {
+            $name += " *"
+        }
+        elseif ($status.Contains("Changes to be committed:")) {
+            $name += " +"
+        }
+        Write-Host("(" + $name + ") ") -nonewline -ForegroundColor Red
+    }
     return "> "
 }
 
