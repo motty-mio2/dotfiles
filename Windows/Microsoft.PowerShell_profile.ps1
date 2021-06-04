8e4890b117ec09c0c04fe2a84f5b91f4931bf264
motty-mio2 <motty.mio2@gmail.com>
2021-06-04 15:03:48 +0900

    delete old files for main branch

--- Windows/Microsoft.PowerShell_profile.ps1
+++
@@ -1,9 +0,0 @@
-Get-ChildItem -Path (Join-Path (Split-Path $profile -Parent) "\conf\") -Name -Recurse -Include *.ps1 | ForEach-Object -Process {
-    Invoke-Expression (". " + (Join-Path (Split-Path $profile -Parent) "\conf\") + $_)
-}
-
-if (Test-Path (Join-Path (Split-Path $profile -Parent) "\addon\")) {
-    Get-ChildItem -Path (Join-Path (Split-Path $profile -Parent) "\addon\") -Name -Recurse -Include *.ps1 | ForEach-Object -Process {
-        Invoke-Expression (". " + (Join-Path (Split-Path $profile -Parent) "\addon\") + $_)
-    }
-}
