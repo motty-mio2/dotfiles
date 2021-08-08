7ab3bd84c720cc1188200da0812db7914961f90a
motty.mio2 <motty.mio2@gmail.com>
2021-08-09 02:33:10 +0900

    fix install repo

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,4 +1,4 @@
-sudo sed -i.bak -e "s%http://archive.ubuntu.com/ubuntu/%http://ub/linux/ubuntu/archive/%g" /etc/apt/sources.list
+sudo sed -i.bak -e "s%http://archive.ubuntu.com/ubuntu/http://ftp.iij.ad.jp/pub/linux/ubuntu/archive/%g" /etc/apt/sources.list
 #sed -e "s/# set constantshow/set constantshow/" -e "s/# set linenumbers/set linenumbers/" -e "s/# set matchbrackets/set matchbrackets/" -e "s/# set mouse/set mouse/" -e "s/# set nonewlines/set nonewlines/" -e "s/# set tabsize 8/set tabsize 4/" -e "s/# set tabstospaces/set tabstospaces/" -e "s/# set trimblanks/set trimblanks/" -i ~/.nanorc
 # sed -ie '$a set constantshow\nset linenumbers\nset historylog' /etc/nanorc && \
 sudo apt-get update
