79f4e0a5402db69c0dfbb824585a9dd858efae79
motty.mio2 <motty.mio2@gmail.com>
2021-11-17 22:46:44 +0900

    add git setting

--- scoop.ps1
+++ scoop.ps1
@@ -17,6 +17,7 @@ scoop alias add backup 'scoop export > ~\scoop.txt'
 
 git config --global core.autocrlf input
 git config --global init.defaultBranch main
+git config pull.rebase false
 
 # (Get-Content ~\.nanorc) | % { $_ -replace "# set constantshow", "set constantshow" } | Set-Content ~\.nanorc
 
