823c42fd2f4ccec57f5ad87ec712984cd5a37128
motty.mio2 <motty.mio2@gmail.com>
2021-11-26 20:30:10 +0900

    add settings for wsl

--- conf/wsl.sh
+++ conf/wsl.sh
@@ -1,1 +1,9 @@
-export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
+# export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
+whome=$(wslpath $(wslvar USERPROFILE))
+
+export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
+ss -a | grep -q $SSH_AUTH_SOCK
+if [ $? -ne 0   ]; then
+    rm -f $SSH_AUTH_SOCK
+    ( setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"$whome/go/bin/npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork & ) >/dev/null 2>&1
+fi
--- setup.sh
+++ setup.sh
@@ -13,10 +13,18 @@ echo "source ~/.profile" >> ~/.zshrc
 echo "source ${HOME}/conf/zsh.sh" >> ~/.zshrc
 echo "source ${HOME}/conf/bash.sh" >> ~/.bashrc
 
+# wsl setup
 if [ $(uname -r | sed -n 's/.*\( *Microsoft *\).*/\1/ip') ];
 then
-        echo "source ${HOME}/conf/wsl.sh" >> ~/.zshrc
-        echo "source ${HOME}/conf/wsl.sh" >> ~/.bashrc
+    whome=$(wslpath $(wslvar USERPROFILE))
+    sudo apt-get install -y golang
+    go get -d github.com/jstarks/npiperelay
+    GOOS=windows go build -o "${whome}/go/bin/npiperelay.exe" github.com/jstarks/npiperelay
+    sudo apt-get purge -y golang
+    rm -rf ~/go
+    ln -s whome ~/whome
+    echo "source ${HOME}/conf/wsl.sh" >> ~/.zshrc
+    echo "source ${HOME}/conf/wsl.sh" >> ~/.bashrc
 fi
 
 echo "is this Server machine? [Y/n]"
