bf547130e94313149550999bd1b26538e538bf7e
motty.mio2 <motty.mio2@gmail.com>
2021-12-09 20:28:31 +0900

    update environment

--- conf/env.ps1
+++ conf/env.ps1
@@ -1,4 +1,14 @@
 $env:DISPLAY = "localhost:0.0"
-$env:PATH = $env:USERPROFILE + "\.poetry\bin" + ";" + "\scoop\apps\pyenv\current\pyenv-win\shims" + ";" + $env:PATH
-$env:PYENV = $env:USERPROFILE + "\scoop\apps\pyenv\current\pyenv-win"
-$env:PYENV_HOME = $env:USERPROFILE + "\scoop\apps\pyenv\current\pyenv-win"
+
+$env:PATH = $env:USERPROFILE + "\.pyenv\pyenv-win\bin" + ";" + $env:PATH
+$env:PATH = $env:USERPROFILE + "\.pyenv\pyenv-win\shims" + ";" + $env:PATH
+$env:PATH = $env:USERPROFILE + "\.poetry\bin" + ";" + $env:PATH
+$env:PATH = $env:USERPROFILE + "\go\bin" + ";" + $env:PATH
+
+# PYENV
+$env:PYENV = $env:USERPROFILE + "\.pyenv\pyenv-win\"
+$env:PYENV_ROOT = $env:USERPROFILE + "\.pyenv\pyenv-win\"
+$env:PYENV_HOME = $env:USERPROFILE + "\.pyenv\pyenv-win\"
+
+# Poetry
+poetry config virtualenvs.in-project true
