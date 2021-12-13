89c94e07cb9066ee79ab720a775d4c8cb24fdbeb
motty.mio2 <motty.mio2@gmail.com>
2021-12-13 21:26:16 +0900

    delete addon option

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,11 +3,3 @@ $configure_folder = Split-Path $profile -Parent
 Get-ChildItem -Path (Join-Path $configure_folder "\conf\") -Name -Recurse -Include *.ps1 | ForEach-Object -Process {
     Invoke-Expression (". " + (Join-Path $configure_folder "\conf\") + $_)
 }
-
-
-$addon_folder = (Join-Path $configure_folder "\addon\")
-if (Test-Path $addon_folder) {
-    Get-ChildItem -Path $addon_folder -Name -Recurse -Include *.ps1 | ForEach-Object -Process {
-        Invoke-Expression (". " + $addon_folder + $_)
-    }
-}
