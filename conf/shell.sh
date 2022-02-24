8afb92355d98774e38cb9267f6f093326b7939a7
motty.mio2 <motty.mio2@gmail.com>
2022-02-24 11:55:08 +0900

    update cats alias

--- conf/shell.sh
+++ conf/shell.sh
@@ -21,7 +21,7 @@ function mkcd() {
 }
 
 function cats() {
-    cat $1 | \grep -v "^#" | \grep -v "^$"
+    cat $1 | grep -vE "^#" | grep -vE "^$" | grep -vE "^( )+#"
 }
 
 export PIPENV_VENV_IN_PROJECT=1
