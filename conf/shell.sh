f6a294176bc33798607028727c736139f4005c6e
motty.mio2 <motty.mio2@gmail.com>
2022-02-26 13:58:13 +0900

    update re expression

--- conf/shell.sh
+++ conf/shell.sh
@@ -21,7 +21,7 @@ function mkcd() {
 }
 
 function cats() {
-    cat $1 | grep -vE "^#" | grep -vE "^$" | grep -vE "^( )+#"
+    cat $1 | grep -vE "^#" | grep -vE "^( )*#"
 }
 
 export PIPENV_VENV_IN_PROJECT=1
