a7401ac328cba583a37bbccd97e4bf508db068f8
motty.mio2 <motty.mio2@gmail.com>
2022-06-07 21:14:53 +0900

    add comspec selector (deprecated)

--- conf/env.ps1
+++ conf/env.ps1
@@ -6,3 +6,11 @@ $env:PATH = $env:USERPROFILE + "\.pyenv\pyenv-win\shims" + ";" + $env:PATH
 $env:PYENV = $env:USERPROFILE + "\.pyenv\pyenv-win\"
 $env:PYENV_HOME = $env:USERPROFILE + "\.pyenv\pyenv-win\"
 $env:PYENV_ROOT = $env:USERPROFILE + "\.pyenv\pyenv-win\"
+
+
+# try {
+#     $ENV:COMSPEC = (Get-Command pwsh -ErrorAction Stop).Definition
+# }
+# catch {
+#     $ENV:COMSPEC = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.EXE"
+# }
