b8a32bfc0255b9ea4334c596a79c07cb04048e81
motty.mio2 <motty.mio2@gmail.com>
2022-07-23 23:38:03 +0900

    updated wsl git

--- conf/wsl.sh
+++ conf/wsl.sh
@@ -7,10 +7,9 @@ if [ -z "${SSH_AGENT_PID}" ]; then
 fi
 
 export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
-ss -a | grep -q "$SSH_AUTH_SOCK"
-if [ $? -ne 0   ]; then
+if ss -a | grep -q "$SSH_AUTH_SOCK"; then
     rm -f "$SSH_AUTH_SOCK"
-    ( setsid socat UNIX-LISTEN:"$SSH_AUTH_SOCK",fork EXEC:"$whome/go/bin/npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork & ) >/dev/null 2>&1
+    ( setsid socat UNIX-LISTEN:"$SSH_AUTH_SOCK",fork EXEC:"$HOME/whome/go/bin/npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork & ) >/dev/null 2>&1
 fi
 
 function open() {
