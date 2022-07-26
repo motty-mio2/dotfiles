e6914f0bd31aa9c12a1d9b474532001abca95c59
motty.mio2 <motty.mio2@gmail.com>
2022-07-26 13:50:15 +0900

    update setup settings for linux

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+script_directory=$(cd $(dirname $0); pwd)
+conf_directory="$HOME/.config/nvim"
+mkdir -p $conf_directory
+
+ln -s "${script_directory}/config/vim" "${HOME}/.vim"
+ln -s "${script_directory}/config/init.lua" "${conf_directory}/init.lua"
+
