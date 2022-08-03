ab7b246f319ff21de519146de0367b93a15df9ec
motty.mio2 <motty.mio2@gmail.com>
2022-08-03 13:30:00 +0900

    update format

--- conf/server.sh
+++ conf/server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #export $SSH_AUTH_SOCK=""
 
-if [ -z "$VSCODE_GIT_ASKPASS_NODE" ]; then
-   alias code="${VSCODE_GIT_ASKPASS_NODE%/*}/bin/remote-cli/code"
+if [ -n "$VSCODE_GIT_ASKPASS_NODE" ]; then
+    alias code='${VSCODE_GIT_ASKPASS_NODE%/*}/bin/remote-cli/code'
 fi
