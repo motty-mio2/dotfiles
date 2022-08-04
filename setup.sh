7b1b550b87fb84fa232fbb0c4a191a9de87773be
motty.mio2 <motty.mio2@gmail.com>
2022-08-05 00:23:14 +0900

    update for mason

--- setup.sh
+++ setup.sh
@@ -2,8 +2,14 @@
 
 script_directory=$(cd $(dirname $0); pwd)
 conf_directory="$HOME/.config/nvim"
-mkdir -p $conf_directory
+mkdir -p "$conf_directory"
 
 ln -s "${script_directory}/config/vim" "${HOME}/.vim"
 ln -s "${script_directory}/config/init.lua" "${conf_directory}/init.lua"
 
+
+mkdir -p ~/.cache/dein
+cd ~/.cache/dein || exit
+
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+sh ./installer.sh ~/.cache/dein
