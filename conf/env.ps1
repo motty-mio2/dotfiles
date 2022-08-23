43402df2f4960db6b2a2ad3cac031a110f7514b8
motty.mio2 <motty.mio2@gmail.com>
2022-08-23 20:59:42 +0900

    del python setting

--- conf/env.ps1
+++ conf/env.ps1
@@ -1,14 +1,4 @@
 $env:DISPLAY = "localhost:0.0"
-$env:PATH = $env:USERPROFILE + "\AppData\Roaming\Python\Scripts" + ";" + $env:PATH
-$env:PATH = $env:USERPROFILE + "\.pyenv\pyenv-win\bin" + ";" + $env:PATH
-$env:PATH = $env:USERPROFILE + "\.pyenv\pyenv-win\shims" + ";" + $env:PATH
-
-$env:PYENV = $env:USERPROFILE + "\.pyenv\pyenv-win\"
-$env:PYENV_HOME = $env:USERPROFILE + "\.pyenv\pyenv-win\"
-$env:PYENV_ROOT = $env:USERPROFILE + "\.pyenv\pyenv-win\"
-
-$SVLINT_CONFIG = $env:USERPROFILE + "\.config\svls\.svlint.toml"
-$env:SVLINT_CONFIG = $env:USERPROFILE + "\.config\svls\.svlint.toml"
 
 # try {
 #     $ENV:COMSPEC = (Get-Command pwsh -ErrorAction Stop).Definition
