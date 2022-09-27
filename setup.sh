906744db1ccba39fbcc9bbfb11518dad6a4ce792
motty.mio2 <motty.mio2@gmail.com>
2022-09-27 16:11:00 +0900

    re-ln config directory

--- setup.sh
+++ setup.sh
@@ -7,18 +7,6 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim\ ~/.local/share/nv
 
 # nvim config directory
 conf_directory="$HOME/.config/nvim"
-mkdir -p "$conf_directory"
 
 ln -s "${script_directory}/config/vim" "${HOME}/.vim"
-ln -s "${script_directory}/config/init.lua" "${conf_directory}/init.lua"
-
-
-mkdir -p ~/.cache/dein
-cd ~/.cache/dein || exit
-
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-sh ./installer.sh ~/.cache/dein
-
-brew install zk # markdown
-node install --location=global pyright # Python
-cargo install svls # SystemVerilog
+ln -s "${script_directory}/config/nvim" "${conf_directory}"
