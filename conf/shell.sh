4034eba20d30b5006adb0635a3bba184e3bfaee0
motty.mio2 <motty.mio2@gmail.com>
2022-10-25 16:43:23 +0900

    merge common config

--- conf/shell.sh
+++ conf/shell.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/sh
 
+export XDG_CONFIG_HOME="$HOME/.config"
+
+# Rust
+if [ -d "$HOME/.cargo/bin" ] ; then
+    export PATH="$HOME/.cargo/bin:$PATH"
+    RUSTC_WRAPPER="$(which sccache)"
+    export RUSTC_WRAPPER
+fi
 alias aupdate='sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y'
 alias bupdate='brew update && brew upgrade'
 alias zupdate='sudo zypper ref && sudo zypper update -y'
