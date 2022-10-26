e27259e38526e2cdba425580e3be4578faf752e6
motty.mio2 <motty.mio2@gmail.com>
2022-10-26 17:49:39 +0900

    update sccache dependency

--- conf/shell.sh
+++ conf/shell.sh
@@ -5,8 +5,10 @@ export XDG_CONFIG_HOME="$HOME/.config"
 # Rust
 if [ -d "$HOME/.cargo/bin" ] ; then
     export PATH="$HOME/.cargo/bin:$PATH"
-    RUSTC_WRAPPER="$(which sccache)"
-    export RUSTC_WRAPPER
+    if [ -f "$HOME/.cargo/bin/sccache" ] ; then
+        RUSTC_WRAPPER="$HOME/.cargo/bin/sccache"
+        export RUSTC_WRAPPER
+    fi
 fi
 alias aupdate='sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y'
 alias bupdate='brew update && brew upgrade'
