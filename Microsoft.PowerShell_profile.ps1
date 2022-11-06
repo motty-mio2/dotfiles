062a6b97a7f20019396dd4176b618c268ab3e275
motty.mio2 <motty.mio2@gmail.com>
2022-11-06 17:05:07 +0900

    add winget completions

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,3 +3,13 @@ $configure_folder = Split-Path $profile -Parent
 Get-ChildItem -Path ($configure_folder + "\conf\") -Recurse -Include *.ps1 | ForEach-Object -Process {
     Invoke-Expression (". " + $_)
 }
+
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+        [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+        $Local:word = $wordToComplete.Replace('"', '""')
+        $Local:ast = $commandAst.ToString().Replace('"', '""')
+        winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
