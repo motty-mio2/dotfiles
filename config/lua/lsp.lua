6fb48be0edbaf5ce36e1c98e42ce6a90ff4e43e6
motty.mio2 <motty.mio2@gmail.com>
2022-12-07 14:34:17 +0900

    update compatibility

--- config/lua/lsp.lua
+++ config/lua/lsp.lua
@@ -20,7 +20,7 @@ require('mason-lspconfig').setup_handlers({function(server)
         --   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
         --   vim.cmd 'autocmd BufWritePre * lua vim.lsp.buf.formatting_sync(nil, 1000)'
         -- end,
-        capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+        capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
     }
     require('lspconfig')[server].setup(opt)
 end})
