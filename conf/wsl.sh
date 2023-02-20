7bb5191446016f2dc8dac21a9c84396f24b7c856
motty <motty.mio2@gmail.com>
2023-02-20 15:32:34 +0900

    change ssh agent base

--- conf/wsl.sh
+++ conf/wsl.sh
@@ -1,16 +1,7 @@
 #!/usr/bin/env bash
 # export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
-#whome=$(wslpath "$(wslvar USERPROFILE)")
 
-if [ -z "${SSH_AGENT_PID}" ]; then
-    eval "$(ssh-agent)" 1>/dev/null
-fi
-
-export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
-if ! ss -a | grep -q "$SSH_AUTH_SOCK" ; then
-    rm -f "$SSH_AUTH_SOCK"
-    ( setsid socat UNIX-LISTEN:"$SSH_AUTH_SOCK",fork EXEC:"$HOME/whome/go/bin/npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork & ) >/dev/null 2>&1
-fi
+eval $("$HOME"/.local/bin/wsl2-ssh-agent)
 
 open() {
     if [ $# != 1 ]; then
