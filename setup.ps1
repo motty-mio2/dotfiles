301f3c95a7dfeccd4405ba4ee77aba5c759b6767
motty <motty.mio2@gmail.com>
2023-03-01 21:30:19 +0900

    delete packer install script

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,3 @@
-# install packer.nvim
-git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
-
 # nvim config directory
 New-Item -ErrorAction Ignore -ItemType Directory -Path '~\AppData\Local\'
 
