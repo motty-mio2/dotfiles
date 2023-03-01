875e77bbb2ca93469c876a02b2ba7dd07a717e55
motty <motty.mio2@gmail.com>
2023-03-01 20:00:10 +0900

    replace Mason with self patched

--- config/lua/plugins.lua
+++ config/lua/plugins.lua
@@ -1,7 +1,7 @@
 require('packer').startup(function()
     use 'wbthomason/packer.nvim'
 
-    use 'williamboman/mason.nvim'
+    use {'motty-mio2/mason.nvim', "hotfix/npm_exe"}
     use 'williamboman/mason-lspconfig.nvim'
     use 'neovim/nvim-lspconfig'
     use 'WhoIsSethDaniel/mason-tool-installer.nvim'
