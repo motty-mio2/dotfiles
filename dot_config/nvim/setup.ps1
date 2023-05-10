f66dd362aa60b11045ded8d2cb9f2eb8b7db2a9c
motty <motty.mio2@gmail.com>
2023-05-10 22:15:31 +0900

    apply chezmoi to nvim

--- dot_config/nvim/setup.ps1
+++
@@ -1,7 +0,0 @@
-# nvim config directory
-New-Item -ErrorAction Ignore -ItemType Directory -Path '~\AppData\Local\'
-
-New-Item -ErrorAction Ignore -ItemType SymbolicLink -Path '~\AppData\Local\' -Name 'nvim' -Value $(Join-Path $PSScriptRoot "\config\")
-
-npm install -g tree-sitter tree-sitter-cli
-
