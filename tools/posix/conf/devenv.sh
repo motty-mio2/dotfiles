b47a005b1f9c4cc9448d115818bee3360a703f1d
motty <motty.mio2@gmail.com>
2023-05-11 01:18:43 +0900

    migrate from develop environments

---
+++ tools/posix/conf/devenv.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# Pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+
+# Poetry
+export PATH="$HOME/.poetry/bin:$PATH" # old
+export PATH="$HOME/.local/share/pypoetry:$PATH" # new
+
+# Python
+alias pytest="pytest -s"
+
+# svlint
+export SVLINT_CONFIG="$HOME/.config/svls/.svlint.toml"
+
+# Verilog
+alias iv="iverilog"
+alias ivx="iverilog -g2012"
+
+# Volta
+export VOLTA_HOME="$HOME/.volta"
+export PATH="$VOLTA_HOME/bin:$PATH"
+
+alias doc='docker compose'
+alias rsmv='\rsync -avhPR --remove-source-files'
+
+ghf () {
+    gh repo clone "$(gh repo list -L 10000 | fzf | awk '{print $1}' )"
+}
