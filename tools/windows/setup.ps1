3dccb00ac5490a264eb416fd4180949e4caf85e3
motty <motty.mio2@gmail.com>
2023-05-11 02:01:03 +0900

    update windows install script

--- tools/windows/setup.ps1
+++ tools/windows/setup.ps1
@@ -1,12 +1,28 @@
-New-Item -ItemType Directory -Path '~\Documents\PowerShell\conf'
-Get-ChildItem -Path (Join-Path $PSScriptRoot "\conf\") -Name -Recurse | ForEach-Object -Process {
-    New-Item -ItemType SymbolicLink -Path '~\Documents\PowerShell\conf\' -Name $_ -Target $(Join-Path $PSScriptRoot "conf\" $_)
-}
-
-New-Item -ItemType SymbolicLink -Path '~\Documents' -Name 'WindowsPowerShell' -Target (Join-Path $HOME '\Documents\PowerShell')
-
-Copy-Item $(Join-Path $PSScriptRoot "Microsoft.PowerShell_profile.ps1") '~\Documents\PowerShell'
-
-New-Item -ItemType SymbolicLink -Path '~\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' -Name config.xlaunch -Target $(Join-Path $PSScriptRoot "config.xlaunch")
-
 Install-Module PSReadLine -Force
+
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+
+Invoke-WebRequest -useb get.scoop.sh | Invoke-Expression
+scoop reset
+
+scoop install git 7zip sudo nano
+scoop bucket add extras
+scoop bucket add github-gh
+scoop bucket add java
+scoop bucket add viewer
+scoop bucket add nonportable
+scoop bucket add versions
+scoop bucket add nerd-font
+scoop bucket add hdl https://github.com/motty-mio2/mio2_bucket.git
+
+scoop alias add upgrade 'scoop update && scoop update *' 'update all'
+scoop alias add backup 'scoop export > ~\scoop.txt'
+scoop alias add reinstall "scoop uninstall {0}; scoop install {0}"
+
+scoop install oh-my-posh
+scoop install posh-git
+scoop install psfzf
+
+sudo sc config "Audiosrv" start= auto
+
+winget install Microsoft.OpenSSH.Beta --override ADDLOCAL=Client
