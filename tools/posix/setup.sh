d33f0faa56617e922fcd003ed91b50d85c94c899
motty <motty.mio2@gmail.com>
2023-05-11 01:52:38 +0900

    update linux setup script

--- tools/posix/setup.sh
+++ tools/posix/setup.sh
@@ -7,22 +7,56 @@ if type "apt" > /dev/null 2>&1; then
     sudo apt-get update
     sudo apt-get upgrade -qy
     sudo apt-get install -qy byobu curl git nano screen unar wget zsh libssl-dev build-essential golang
+    # Pyenv Dependency
+    sudo apt-get install -y \
+    build-essential curl llvm make tk-dev xz-utils wget \
+    libbz2-dev libffi-dev liblzma-dev libncursesw5-dev libreadline-dev \
+    libsqlite3-dev libssl-dev libxml2-dev libxmlsec1-dev zlib1g-dev \
+    python3-dev python3-venv python-is-python3
     
     elif type "pacman" > /dev/null 2>&1; then
     sudo pacman -Sy --noconfirm nano wget zsh git unarchiver byobu curl screen pkg-config
+    # Pyenv Dependency
+    sudo pacman -S --needed base-devel openssl zlib xz tk
     
     elif type "dnf" > /dev/null 2>&1; then
     echo "RHEL Mode"
     sudo dnf -q -y groupinstall "Development Tools"
-    sudo dnf -q -y install openssl-devel
+    # Pyenv dependency
+    if [ -f "/etc/fedora-release" ]; then
+        sudo dnf -q -y install make gcc patch zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz-devel libuuid-devel gdbm-devel libnsl2-devel
+    else
+        sudo yum -q -y install gcc make patch zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz-devel python39
+    fi
+    
+    elif type "zypper" > /dev/null 2>&1; then
+    zypper install gcc automake bzip2 libbz2-devel xz xz-devel openssl-devel ncurses-devel \
+    readline-devel zlib-devel tk-devel libffi-devel sqlite3-devel libgdbm-devel make findutils
 else
     echo "None"
 fi
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Pyenv Install
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+~/.pyenv/bin/pyenv install 3.10.11
+~/.pyenv/bin/pyenv global 3.10.11
 
+# Poetry Install
+curl -sSL https://install.python-poetry.org | /usr/bin/python3 -
+~/.local/bin/poetry config virtualenvs.in-project true
+
+# Volta Install
+curl https://get.volta.sh | bash
+~/.volta/bin/volta install node@lts
+
+# Use Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/home/linuxbrew/.linuxbrew/bin/brew install chezmoi fzf gh neovim
+
+# Use Rust
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 
-~/.cargo/bin/cargo install cargo-update sccache sheldon
-/home/linuxbrew/.linuxbrew/bin/brew install chezmoi
+~/.cargo/bin/cargo install cargo-update sccache sheldonbat fd-find git-ignore-generator
+
+# Use chezmoi
 /home/linuxbrew/.linuxbrew/bin/chezmoi init motty-mio2
