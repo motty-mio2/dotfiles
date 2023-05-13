8ccf8b18021dcdec0a7db41ec3fe16ac769b8d20
motty <motty.mio2@gmail.com>
2023-05-14 00:25:45 +0900

    powershell omp starship select

--- tools/windows/conf/prompt.ps1
+++ tools/windows/conf/prompt.ps1
@@ -1,6 +1,3 @@
-# oh-my-posh init pwsh --config $PSScriptRoot\theme.omp.json | Invoke-Expression
-# Enable-PoshTransientPrompt
-
 Import-Module PSReadLine
 Import-Module (Join-Path $HOME "\scoop\modules\posh-git")
 
@@ -8,3 +5,21 @@ Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadlineKeyHandler -Key ctrl+d -Function DeleteCharOrExit
 
 Set-PSReadLineOption -PredictionViewStyle InlineView
+
+
+#Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t'
+#Set-PsFzfOption -PSReadlineChordReverseHistory 'Ctrl+r'
+#Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+
+
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+    [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+    $Local:word = $wordToComplete.Replace('"', '""')
+    $Local:ast = $commandAst.ToString().Replace('"', '""')
+    winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
+}
+
+$host.ui.RawUI.WindowTitle = "$env:USERNAME@$env:COMPUTERNAME"
