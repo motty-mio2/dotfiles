d5cd641d033b14ac4d28f19428ec0964ef3152da
motty <motty.mio2@gmail.com>
2023-05-16 18:51:10 +0900

    add wezterm

--- tools/windows/setup.ps1
+++ tools/windows/setup.ps1
@@ -17,7 +17,7 @@ scoop bucket add versions
 scoop bucket add nerd-font
 scoop bucket add hdl https://github.com/motty-mio2/mio2_bucket.git
 
-scoop install 7zip bat bitwarden-cli chezmoi fd fzf gh git gsudonano less make neovim oh-my-posh posh-git psfzf python svlint svls volta wget which
+scoop install 7zip bat bitwarden-cli chezmoi fd fzf gh git gsudonano less make neovim oh-my-posh posh-git psfzf python svlint svls volta wezterm wget which
 
 scoop alias add upgrade 'scoop update && scoop update *' 'update all'
 scoop alias add backup 'scoop export > ~\scoop.txt'
