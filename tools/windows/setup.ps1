db4d20ff61732f41394542f78624a0c751ec2440
motty <motty.mio2@gmail.com>
2023-05-16 18:38:40 +0900

    update windows install script

--- tools/windows/setup.ps1
+++ tools/windows/setup.ps1
@@ -1,15 +1,13 @@
-Install-Module PSReadLine -Force
-
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 
 powershell -NoProfile Install-Module -Name PSReadLine -Scope CurrentUser -Force
 pwsh -NoProfile Install-Module -Name PSReadLine -Scope CurrentUser -Force
 
+# Install Scoop
 Invoke-WebRequest -useb get.scoop.sh | Invoke-Expression
-
 scoop reset
 
-scoop install git 7zip gsudo nano
 scoop bucket add extras
 scoop bucket add github-gh
 scoop bucket add java
@@ -19,13 +17,22 @@ scoop bucket add versions
 scoop bucket add nerd-font
 scoop bucket add hdl https://github.com/motty-mio2/mio2_bucket.git
 
+scoop install 7zip bat bitwarden-cli chezmoi fd fzf gh git gsudonano less make neovim oh-my-posh posh-git psfzf python svlint svls volta wget which
+
 scoop alias add upgrade 'scoop update && scoop update *' 'update all'
 scoop alias add backup 'scoop export > ~\scoop.txt'
 scoop alias add reinstall "scoop uninstall {0}; scoop install {0}"
 
-scoop install oh-my-posh
-scoop install posh-git
-scoop install psfzf
+# Pyenv Install
+Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/pyenv-win/pyenv-win/master/pyenv-win/install-pyenv-win.ps1" -OutFile "./install-pyenv-win.ps1"; &"./install-pyenv-win.ps1"
+
+# Poetry Install
+(Invoke-WebRequest -Uri https://install.python-poetry.org -UseBasicParsing).Content | python -
+~/AppData/Roaming/Python/Scripts/poetry.exe config virtualenvs.in-project true
+
+# Volta Install
+volta install node@lts
+
 
 sudo sc config "Audiosrv" start= auto
 
