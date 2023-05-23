30d7dcb58cf923fd6525c1243bbf7a2d95525f3d
motty <motty.mio2@gmail.com>
2023-05-23 18:01:17 +0900

    fix wsl2-ssh-agent mode

--- tools/posix/conf/wsl.sh
+++ tools/posix/conf/wsl.sh
@@ -1,16 +1,29 @@
 #!/usr/bin/env bash
 # export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
+
 if grep -q microsoft /proc/version ; then
     eval '$("$HOME"/.local/bin/wsl2-ssh-agent)'
-    
+    agent_binary="$HOME/.local/bin/wsl2-ssh-agent"
+    if [ ! -f "$agent_binary" ]; then
+        curl -L -O https://github.com/mame/wsl2-ssh-agent/releases/latest/download/wsl2-ssh-agent --output "$agent_binary"
+        chmod 755 "$agent_binary"
+    fi
+
+    eval "$("$agent_binary")"
+
     open() {
         if [ $# != 1 ]; then
             explorer.exe .
         else
             if [ -e "$1" ]; then
                 cmd.exe /c start "$(wslpath -w "$1")" 2> /dev/null
+
             else
-                echo "open: $1 : No such file or directory"
+                if [ -e "$1" ]; then
+                    cmd.exe /c start "$(wslpath -w "$1")" 2> /dev/null
+                else
+                    echo "open: $1 : No such file or directory"
+                fi
             fi
         fi
     }
