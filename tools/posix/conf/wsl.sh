ed11e20ba3bc219251a3bd693d2e7d0c8fc69af8
motty <motty.mio2@gmail.com>
2023-05-23 12:36:29 +0900

    check if wsl

--- tools/posix/conf/wsl.sh
+++ tools/posix/conf/wsl.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 # export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
-
-eval '$("$HOME"/.local/bin/wsl2-ssh-agent)'
-
-open() {
-    if [ $# != 1 ]; then
-        explorer.exe .
-    else
-        if [ -e "$1" ]; then
-            cmd.exe /c start "$(wslpath -w "$1")" 2> /dev/null
+if grep -q microsoft /proc/version ; then
+    eval '$("$HOME"/.local/bin/wsl2-ssh-agent)'
+    
+    open() {
+        if [ $# != 1 ]; then
+            explorer.exe .
         else
-            echo "open: $1 : No such file or directory"
+            if [ -e "$1" ]; then
+                cmd.exe /c start "$(wslpath -w "$1")" 2> /dev/null
+            else
+                echo "open: $1 : No such file or directory"
+            fi
         fi
-    fi
-}
+    }
+fi
