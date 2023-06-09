069c708c7489906c8fe6fa471980e3483bc99c93
motty <motty.mio2@gmail.com>
2023-06-09 15:51:49 +0900

    update svlint setting

--- tools/windows/conf/devenv.ps1
+++ tools/windows/conf/devenv.ps1
@@ -1,7 +1,3 @@
-# svlint
-$env:SVLINT_CONFIG = $env:USERPROFILE + "\.config\svls\.svlint.toml"
-$SVLINT_CONFIG = $env:USERPROFILE + "\.config\svls\.svlint.toml"
-
 # Python
 Set-Alias python3 python
 Set-Alias pip3 pip
@@ -31,41 +27,31 @@ function Set-Environemt-Path {
     }
 }
 
+
 function Set-Poetry-Path {
     param (
         $POETRY_DIR = $env:USERPROFILE + "\AppData\Roaming\Python\Scripts"
     )
-    $user_path = [System.Environment]::GetEnvironmentVariable('PATH', "User")
+    Set-Environemt-Path -ENV_NAME "PATH" -ENV_VALUE $POETRY_DIR
 
-    if (-Not $user_path.Contains( $POETRY_DIR )) {
-        [System.Environment]::SetEnvironmentVariable("PATH", "$POETRY_DIR" + ";" + "$user_path", "User")
-    }
 }
 
+function Set-SVLINT-PATH {
+    param (
+        $SVLINT_CONFIG = $env:USERPROFILE + "\.config\svlint\svlint.toml"
+    )
+
+    Set-Environemt-Path -ENV_NAME "SVLINT_CONFIG" -ENV_VALUE $SVLINT_CONFIG
+}
 function Set-Pyenv-Path {
     param (
         $PYENV_DIR = $env:USERPROFILE + "\.pyenv\pyenv-win"
     )
 
-    foreach ($target in @( "PYENV", "PYENV_HOME", "PYENV_ROOT")) {
-        $tmp_path = [System.Environment]::GetEnvironmentVariable("$target", "User")
+    Set-Environemt-Path -ENV_NAME "PYENV" -ENV_VALUE $PYENV_DIR
+    Set-Environemt-Path -ENV_NAME "PYENV_HOME" -ENV_VALUE $PYENV_DIR
+    Set-Environemt-Path -ENV_NAME "PYENV_ROOT" -ENV_VALUE $PYENV_DIR
 
-        if ( ($null -eq $tmp_path) -Or ($tmp_path -ne $PYENV_DIR )) {
-            [System.Environment]::SetEnvironmentVariable('PYENV', $PYENV_DIR, "User")
-        }
-    }
-
-    $user_path = [System.Environment]::GetEnvironmentVariable('PATH', "User")
-    $pyenv_bin = $PYENV_DIR + "\bin"
-
-    if (-Not $user_path.Contains( $pyenv_bin )) {
-        [System.Environment]::SetEnvironmentVariable("PATH", $pyenv_bin + ";" + $user_path, "User")
-    }
-
-    $user_path = [System.Environment]::GetEnvironmentVariable('PATH', "User")
-    $pyenv_shims = $PYENV_DIR + "\shims"
-
-    if (-Not $user_path.Contains( $pyenv_shims )) {
-        [System.Environment]::SetEnvironmentVariable("PATH", $pyenv_shims + ";" + $user_path, "User")
-    }
+    Set-Environemt-Path -ENV_NAME "PATH" -ENV_VALUE $PYENV_DIR + "\bin"
+    Set-Environemt-Path -ENV_NAME "PATH" -ENV_VALUE $PYENV_DIR + "\shims"
 }
