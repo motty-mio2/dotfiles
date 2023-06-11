ddee4a6bcacdfbbf5e6d303620e890061033df49
motty <motty.mio2@gmail.com>
2023-06-11 17:05:00 +0900

    check directory exist or not

--- tools/posix/conf/envload.sh
+++ tools/posix/conf/envload.sh
@@ -2,11 +2,17 @@
 #!/usr/bin/env bash
 
 dot_private="${HOME}/.config/shell/dotprivate/addon"
-for file in "$dot_private"/*.sh ; do
-    source "${file}"
-done
+if [ -d "$dor_private" ]
+then
+    for file in "$dot_private"/*.sh ; do
+        source "${file}"
+    done
+fi
 
 private_folder="${HOME}/.config/shell/private"
-for file in "$private_folder"/*.sh ; do
-    source "${file}"
-done
+if [ -d "$private_folder" ]
+then
+    for file in "$private_folder"/*.sh ; do
+        source "${file}"
+    done
+fi
