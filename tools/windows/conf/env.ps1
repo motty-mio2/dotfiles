6c75e527107bc86f98951f03b0be8368ef1e96f9
motty <motty.mio2@gmail.com>
2023-06-15 01:59:22 +0900

    update for envload

--- tools/windows/conf/env.ps1
+++ tools/windows/conf/env.ps1
@@ -6,3 +6,13 @@ $env:DISPLAY = "localhost:0.0"
 # catch {
 #     $ENV:COMSPEC = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.EXE"
 # }
+
+$private_folder = $HOME + "\.config\shell\dotprivate\addon"
+Get-ChildItem -Path ($private_folder) -Recurse -Include *.ps1 | ForEach-Object -Process {
+    Invoke-Expression (". " + $_)
+}
+
+$private_folder = $HOME + "\.config\shell\private"
+Get-ChildItem -Path ($private_folder) -Recurse -Include *.ps1 | ForEach-Object -Process {
+    Invoke-Expression (". " + $_)
+}
