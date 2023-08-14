907e125890e6ada9448e5e54a2b080f9ef783529
motty <motty.mio2@gmail.com>
2023-08-14 23:26:48 +0900

    delete poetry and pyenv

--- tools/windows/conf/devenv.ps1
+++ tools/windows/conf/devenv.ps1
@@ -46,18 +46,9 @@ function Set-Environemt-Path {
 }
 
 
-function Set-Poetry-Path {
-    param (
-        $POETRY_DIR = $env:USERPROFILE + "\AppData\Roaming\Python\Scripts"
-    )
-    Set-Environemt-Path -ENV_NAME "PATH" -ENV_VALUE $POETRY_DIR
-
-}
-
 function Install-Rye {
     param (
         $url = "https://github.com/mitsuhiko/rye/releases/latest/download/rye-x86_64-windows.exe",
-        $installDir = "$env:USERPROFILE\.local\bin",
         $RYE_HOME = "$env:USERPROFILE\.local\share\rye",
         $executableName = "rye.exe"
     )
@@ -67,24 +58,28 @@ function Install-Rye {
         New-Item -Path $RYE_HOME -ItemType Directory
     }
 
-    [System.Environment]::SetEnvironmentVariable("RYE_HOME", "$RYE_HOME", "User")
     $env:RYE_HOME = "$RYE_HOME"
 
     # Install Rye
-    if (-not (Test-Path -Path $installDir)) {
-        New-Item -Path $installDir -ItemType Directory
-    }
+    Invoke-WebRequest -Uri $url -OutFile (Join-Path -Path $env:HOME -ChildPath $executableName)
+    & "$env:HOME\$executableName"
 
-    Invoke-WebRequest -Uri $url -OutFile (Join-Path -Path $installDir -ChildPath $executableName)
-    & "$installDir\$executableName"
+    Remove-Item "$env:HOME\$executableName"
 
-    Remove-Item "$installDir\$executableName"
+    $env:PATH = "$RYE_HOME\shims;" + $env:PATH
 
     Set-Environemt-Path -ENV_NAME "PATH" -ENV_VALUE "$RYE_HOME\shims"
-    $env:PATH = "$RYE_HOME\shims;" + $env:PATH
+    [System.Environment]::SetEnvironmentVariable("RYE_HOME", "$RYE_HOME", "User")
 }
 
+function Set-Rye-Path {
+    param (
+        $RYE_HOME = "$env:USERPROFILE\.local\share\rye"
+    )
+    Set-Environemt-Path -ENV_NAME "PATH" -ENV_VALUE "$RYE_HOME\shims"
+    [System.Environment]::SetEnvironmentVariable("RYE_HOME", "$RYE_HOME", "User")
 
+}
 function Install-Rye-Tools {
     param (
         $RYE_ENV = "$env:USERPROFILE\.local\share\rye\shims",
@@ -102,16 +97,4 @@ function Set-SVLINT-PATH {
     )
 
     Set-Environemt-Path -ENV_NAME "SVLINT_CONFIG" -ENV_VALUE $SVLINT_CONFIG
-}
-function Set-Pyenv-Path {
-    param (
-        $PYENV_DIR = $env:USERPROFILE + "\.pyenv\pyenv-win"
-    )
-
-    Set-Environemt-Path -ENV_NAME "PYENV" -ENV_VALUE $PYENV_DIR
-    Set-Environemt-Path -ENV_NAME "PYENV_HOME" -ENV_VALUE $PYENV_DIR
-    Set-Environemt-Path -ENV_NAME "PYENV_ROOT" -ENV_VALUE $PYENV_DIR
-
-    Set-Environemt-Path -ENV_NAME "PATH" -ENV_VALUE $PYENV_DIR + "\bin"
-    Set-Environemt-Path -ENV_NAME "PATH" -ENV_VALUE $PYENV_DIR + "\shims"
 }
