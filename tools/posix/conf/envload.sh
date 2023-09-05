4e3ed0fe194dd680530bfa13af7cbd29abaa0540
motty <motty.mio2@gmail.com>
2023-09-05 14:49:44 +0900

    fix dotprivate settings

--- tools/posix/conf/envload.sh
+++ tools/posix/conf/envload.sh
@@ -1,10 +1,10 @@
-
 #!/usr/bin/env bash
 
 dot_private="${HOME}/.config/shell/dotprivate/addon"
-if [ -d "$dor_private" ]
+if [ -d "$dot_private" ]
 then
     for file in "$dot_private"/*.sh ; do
+        # shellcheck source=/dev/null
         source "${file}"
     done
 fi
@@ -13,6 +13,7 @@ private_folder="${HOME}/.config/shell/private"
 if [ -d "$private_folder" ]
 then
     for file in "$private_folder"/*.sh ; do
+        # shellcheck source=/dev/null
         source "${file}"
     done
 fi
