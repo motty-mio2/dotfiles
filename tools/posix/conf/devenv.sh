e49e0dde62d36d94a52a373eb0513cb9992a6d85
motty <motty.mio2@gmail.com>
2023-09-05 21:08:21 +0900

    update devenv

--- tools/posix/conf/devenv.sh
+++ tools/posix/conf/devenv.sh
@@ -10,6 +10,18 @@ if [ -d "$HOME/go/bin" ] ; then
     export PATH="$HOME/go/bin:$PATH"
 fi
 
+# Pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+if which "pyenv" >/dev/null 2>&1; then
+    eval "$(pyenv init --path)"
+    eval "$(pyenv init -)"
+fi
+
+# Poetry
+export PATH="$HOME/.poetry/bin:$PATH" # old
+export PATH="$HOME/.local/share/pypoetry:$PATH" # new
+
 # Rust
 if [ -d "$HOME/.cargo/bin" ] ; then
     export PATH="$HOME/.cargo/bin:$PATH"
@@ -18,12 +30,16 @@ fi
 # Rye
 export RYE_HOME="$HOME/.local/share/rye"
 if [ -d "$RYE_HOME" ] ; then
+    # shellcheck source=/dev/null
     source "$RYE_HOME/env"
 fi
 
-
 # svlint
 export SVLINT_CONFIG="$HOME/.config/svls/.svlint.toml"
+
+# Verilog
+alias iv="iverilog"
+alias ivx="iverilog -g2012"
 
 # Verilog
 alias iv="iverilog"
@@ -33,6 +49,7 @@ alias ivx="iverilog -g2012"
 export VOLTA_HOME="$HOME/.volta"
 export PATH="$VOLTA_HOME/bin:$PATH"
 
+# Others
 alias doc='docker compose'
 alias rsmv='\rsync -avhPR --remove-source-files'
 
