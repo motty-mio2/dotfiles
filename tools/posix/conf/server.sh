13edb64ff47133bdc944e5d992269345106e2ede
motty <motty.mio2@gmail.com>
2023-09-05 14:22:11 +0900

    update server's script

--- tools/posix/conf/server.sh
+++ tools/posix/conf/server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #export $SSH_AUTH_SOCK=""
 
-if [ -n "$VSCODE_GIT_ASKPASS_NODE" ]; then
+if [ ! -e "$$HOME/.ssh/id*" ] && [ -n "$VSCODE_GIT_ASKPASS_NODE" ]; then
     alias code='${VSCODE_GIT_ASKPASS_NODE%/*}/bin/remote-cli/code'
 fi
