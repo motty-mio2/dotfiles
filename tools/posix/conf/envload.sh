b3e9e626e979d467ff54bdb659c253549b56c607
motty <motty.mio2@gmail.com>
2023-09-10 13:21:58 +0900

    apply formatter

--- tools/posix/conf/envload.sh
+++ tools/posix/conf/envload.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 
 dot_private="${HOME}/.config/shell/dotprivate/addon"
-if [ -d "$dot_private" ]
-then
-    for file in "$dot_private"/*.sh ; do
-        # shellcheck source=/dev/null
-        source "${file}"
-    done
+if [ -d "$dot_private" ]; then
+	for file in "$dot_private"/*.sh; do
+		# shellcheck source=/dev/null
+		source "${file}"
+	done
 fi
 
 private_folder="${HOME}/.config/shell/private"
-if [ -d "$private_folder/*.sh" ]
-then
-    for file in "$private_folder"/*.sh ; do
-        # shellcheck source=/dev/null
-        source "${file}"
-    done
+if [ -d "$private_folder/*.sh" ]; then
+	for file in "$private_folder"/*.sh; do
+		# shellcheck source=/dev/null
+		source "${file}"
+	done
 fi
--- tools/posix/conf/server.sh
+++ tools/posix/conf/server.sh
@@ -2,5 +2,5 @@
 #export $SSH_AUTH_SOCK=""
 
 if [ ! -e "$$HOME/.ssh/id*" ] && [ -n "$VSCODE_GIT_ASKPASS_NODE" ]; then
-    alias code='${VSCODE_GIT_ASKPASS_NODE%/*}/bin/remote-cli/code'
+	alias code='${VSCODE_GIT_ASKPASS_NODE%/*}/bin/remote-cli/code'
 fi
