40bce5ab7d15b77618d353a03ab4d81741519ffe
motty <motty.mio2@gmail.com>
2023-09-11 16:15:19 +0900

    fix code alias on code

--- tools/posix/conf/server.sh
+++ tools/posix/conf/server.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 #export $SSH_AUTH_SOCK=""
 
-if [ ! -e "$$HOME/.ssh/id*" ] && [ -n "$VSCODE_GIT_ASKPASS_NODE" ]; then
-	alias code='${VSCODE_GIT_ASKPASS_NODE%/*}/bin/remote-cli/code'
+if ! ls "$HOME"/.ssh/id* > /dev/null 2>&1 ; then
+	if [ -n "$VSCODE_GIT_ASKPASS_NODE" ]; then
+		alias code='${VSCODE_GIT_ASKPASS_NODE%/*}/bin/remote-cli/code'
+	fi
 fi
