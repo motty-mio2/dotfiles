a73b65a3a9023f3d0603f35f7bc689bbcf16884c
motty <motty.mio2@gmail.com>
2023-09-11 19:17:38 +0900

    replace if ssh exist

--- tools/posix/conf/server.sh
+++ tools/posix/conf/server.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 #export $SSH_AUTH_SOCK=""
 
-if ! ls "$HOME"/.ssh/id* > /dev/null 2>&1 ; then
+id_files=( "$HOME"/.ssh/id* ) # ワイルドカード展開でファイルを取得
+
+if [ ${#id_files[@]} -eq 0 ]; then
 	if [ -n "$VSCODE_GIT_ASKPASS_NODE" ]; then
 		alias code='${VSCODE_GIT_ASKPASS_NODE%/*}/bin/remote-cli/code'
 	fi
