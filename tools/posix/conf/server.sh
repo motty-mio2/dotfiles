a397e393fa040e9f786a8bd7de8dc6e40649325f
motty <motty.mio2@gmail.com>
2023-09-29 14:15:38 +0900

    fix id exist check

--- tools/posix/conf/server.sh
+++ tools/posix/conf/server.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 #export $SSH_AUTH_SOCK=""
 
-if [ ! -e $HOME/.ssh/id* ]; then
+count=$(find "$HOME/.ssh" -name 'id*' 2>/dev/null | wc -l)
+
+if [ "$count" -eq 0 ]; then
 	if [ -n "$VSCODE_GIT_ASKPASS_NODE" ]; then
 		alias code='${VSCODE_GIT_ASKPASS_NODE%/*}/bin/remote-cli/code'
 	fi
