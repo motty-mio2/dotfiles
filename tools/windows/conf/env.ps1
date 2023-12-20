819302d31f5336679109904cd857159600672265
motty <motty.mio2@gmail.com>
2023-12-20 14:33:09 +0900

    recurse search update

--- tools/windows/conf/env.ps1
+++ tools/windows/conf/env.ps1
@@ -1,20 +1,8 @@
 $env:EDITOR = "nvim"
 $env:DISPLAY = "localhost:0.0"
 
-# $env:Path = "$HOME\bin;" + $env:Path
-# try {
-#     $ENV:COMSPEC = (Get-Command pwsh -ErrorAction Stop).Definition
-# }
-# catch {
-#     $ENV:COMSPEC = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.EXE"
-# }
-
-$private_folder = $HOME + "\.config\shell\dotprivate\addon"
-Get-ChildItem -Path ($private_folder) -Recurse -Include *.ps1 | ForEach-Object -Process {
-    Invoke-Expression (". " + $_)
-}
-
-$private_folder = $HOME + "\.config\shell\private"
-Get-ChildItem -Path ($private_folder) -Recurse -Include *.ps1 | ForEach-Object -Process {
-    Invoke-Expression (". " + $_)
+if (Test-Path ($HOME + "\.config\shell\")) {
+    Get-ChildItem -Path ($HOME + "\.config\shell\") -Recurse -Include *.ps1 | ForEach-Object -Process {
+        Invoke-Expression (". " + $_)
+    }
 }
