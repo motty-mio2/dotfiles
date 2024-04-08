239c726df00dfafb08bdc2f68451b228311e0ad5
motty <motty.mio2@gmail.com>
2024-04-09 00:12:05 +0900

    diable config load message

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -18,9 +18,9 @@ else
     end
 end
 
-wezterm.on('window-config-reloaded', function(window, pane)
-    window:toast_notification('wezterm', 'configuration reloaded!', nil, 4000)
-end)
+-- wezterm.on('window-config-reloaded', function(window, pane)
+--     window:toast_notification('wezterm', 'configuration reloaded!', nil, 4000)
+-- end)
 
 return {
     font = wezterm.font("HackGen Console"), -- 自分の好きなフォントいれる
