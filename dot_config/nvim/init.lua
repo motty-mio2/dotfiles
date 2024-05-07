479391a4c74b991347bdfe0aa44533b90ad5c249
motty <motty.mio2@gmail.com>
2024-05-07 12:30:50 +0900

    apply stylua formatter

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -29,16 +29,21 @@ vim.opt.signcolumn = "yes" -- always show the sign column, otherwise it would sh
 
 vim.opt.list = true
 vim.opt.listchars = {
-    tab = '>>',
-    trail = '-',
-    nbsp = '+'
+	tab = ">>",
+	trail = "-",
+	nbsp = "+",
 }
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system(
-        {"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
-         lazypath})
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
