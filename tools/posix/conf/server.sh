ebe2cbde4a14e4121aa1e1a73a8acb9af94c2d1f
motty <motty.mio2@gmail.com>
2024-05-16 14:49:21 +0900

    handl ssh in server.sh

--- tools/posix/conf/server.sh
+++ tools/posix/conf/server.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #export $SSH_AUTH_SOCK=""
 
-count=$(find "$HOME/.ssh" -name 'id*' 2>/dev/null | wc -l)
-
-if [ "$count" -eq 0 ]; then
+if [ -f "$HOME/.ssh/id*" ]; then
+	eval "$(ssh-agent -s)"
+else
 	if [ -n "$VSCODE_GIT_ASKPASS_NODE" ]; then
 		alias code='${VSCODE_GIT_ASKPASS_NODE%/*}/bin/remote-cli/code'
 	fi
