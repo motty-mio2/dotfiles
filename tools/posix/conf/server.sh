f9c96270448b47d5b94898989d6f9c227d8da0c2
motty <motty.mio2@gmail.com>
2024-05-19 22:59:56 +0900

    fix ssh-agent function

--- tools/posix/conf/server.sh
+++ tools/posix/conf/server.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 #export $SSH_AUTH_SOCK=""
 
-if [ -f "$HOME/.ssh/id*" ]; then
-	eval "$(ssh-agent -s)"
+id_files=$(find "$HOME"/.ssh/id* 2>/dev/null | wc -l)
+
+if [ "$id_files" ]; then
+	eval "$(ssh-agent -s)" >/dev/null 2>&1
+	ssh-add "$HOME"/.ssh/id* 2>/dev/null
 else
 	if [ -n "$VSCODE_GIT_ASKPASS_NODE" ]; then
 		alias code='${VSCODE_GIT_ASKPASS_NODE%/*}/bin/remote-cli/code'
