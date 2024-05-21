8b853b7faf43892ef667e074902a494058972c54
motty <motty.mio2@gmail.com>
2024-05-21 19:26:26 +0900

    update neovim setting

---
+++ dot_config/nvim/lua/completion.lua
@@ -0,0 +1,45 @@
+local cmp = require("cmp")
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			vim.fn["vsnip#anonymous"](args.body)
+		end,
+	},
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "nvim_lsp_signature_help" },
+		{ name = "path" },
+		{ name = "buffer" },
+		{ name = "nvim_lua" },
+		{ name = "luasnip" },
+		{ name = "cmdline" },
+		{ name = "git" },
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-p>"] = cmp.mapping.select_prev_item(),
+		["<C-n>"] = cmp.mapping.select_next_item(),
+		["<C-l>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({
+			select = true,
+		}),
+	}),
+	experimental = {
+		ghost_text = true,
+	},
+})
+-- cmp.setup.cmdline('/', {
+--   mapping = cmp.mapping.preset.cmdline(),
+--   sources = {
+--     { name = 'buffer' }
+--   }
+-- })
+-- cmp.setup.cmdline(":", {
+--   mapping = cmp.mapping.preset.cmdline(),
+--   sources = {
+--     { name = "path" },
+--     { name = "cmdline" },
+--   },
+-- })
+--
+--
--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,5 +1,5 @@
 return {
-	{ "nvimtools/none-ls.nvim" },
+	{ "nvimtools/none-ls.nvim", dependencies = { "nvim-lua/plenary.nvim" }, config = true },
 	{ "neovim/nvim-lspconfig" },
 	{ "nvim-treesitter/nvim-treesitter" },
 	{
@@ -14,9 +14,14 @@ return {
 	},
 	{
 		"neanias/everforest-nvim",
-		priority = 1000,
+		version = false,
+		lazy = false,
+		priority = 1000, -- make sure to load this before all the other start plugins
+		-- Optional; default configuration will be used if setup isn't called.
 		config = function()
-			require("everforest").setup()
+			require("everforest").setup({
+				-- Your config here
+			})
 		end,
 	},
 	{
@@ -26,5 +31,18 @@ return {
 	{
 		"nvim-neo-tree/neo-tree.nvim",
 		dependencies = { "nvim-lua/plenary.nvim", "kyazdani42/nvim-web-devicons", "MunifTanjim/nui.nvim" },
+	},
+	{
+		"folke/lazy.nvim",
+	},
+	{
+		"j-hui/fidget.nvim",
+	},
+	{
+		"numToStr/Comment.nvim",
+		opts = {
+			-- add any options here
+		},
+		lazy = false,
 	},
 }
