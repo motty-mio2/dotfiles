ff6e0ffdb92d0ef928f235451337fdd42ecfbf9a
motty <motty.mio2@gmail.com>
2024-06-12 08:59:21 +0900

    move completion into file

--- tools/windows/conf/devenv.ps1
+++ tools/windows/conf/devenv.ps1
@@ -2,34 +2,6 @@
 Set-Alias python3 python
 Set-Alias pip3 pip
 
-# Volta
-try {
-    (& volta completions powershell) | Out-String | Invoke-Expression
-}
-catch {
-}
-
-# gh
-try {
-    (& gh completion -s powershell) | Out-String | Invoke-Expression
-}
-catch {
-}
-
-# chezmoi
-try {
-    (& chezmoi completion powershell) | Out-String | Invoke-Expression
-}
-catch {
-}
-
-# kubectl
-try {
-    (& kubectl completion powershell) | Out-String | Invoke-Expression -ErrorAction SilentlyContinue
-}
-catch {
-}
-
 function ghf {
     gh repo clone $(gh repo list -L 10000 | fzf).Split("`t")[0]
 }
