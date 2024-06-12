7e316be67ace08989478254fdfc162d87166f1ac
motty <motty.mio2@gmail.com>
2024-06-12 09:02:50 +0900

    load powershell completion

--- tools/windows/conf/devenv.ps1
+++ tools/windows/conf/devenv.ps1
@@ -10,3 +10,9 @@ function mygitconfig {
     git config --local user.name "motty"
     git config --local user.email "motty.mio2@gmail.com"
 }
+
+if Test-Path ($HOME + "\Documents\PowerShell\completion") {
+    Get-ChildItem -Path ($HOME + "\Documents\PowerShell\completion") -Recurse -Include *.ps1 | ForEach-Object -Process {
+        Invoke-Expression (". " + $_)
+    }
+}
