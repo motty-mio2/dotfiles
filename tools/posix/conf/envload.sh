0ee41cf33e11c93bc83057346c337442ffb5166d
motty <motty.mio2@gmail.com>
2024-06-27 00:47:50 +0900

    migrate shell under config

--- tools/posix/conf/envload.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -d "${HOME}/.config/shell/" ]; then
-	while read -r f; do
-		# shellcheck source=/dev/null
-		source "$f"
-	done < <(find "${HOME}/.config/shell/" -type f -name "*.sh")
-fi
