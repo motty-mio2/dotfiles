1cbd1b391ca843646ac3d48bb4c90c34698b16b6
motty <motty.mio2@gmail.com>
2024-08-19 00:47:23 +0900

    a

--- flake.nix
+++ flake.nix
@@ -6,23 +6,11 @@
   };
 
   outputs = { self, nixpkgs }: {
+
     packages.x86_64-linux.cli = nixpkgs.legacyPackages.x86_64-linux.buildEnv {
       name = "my-packages-list";
-      paths = with nixpkgs.legacyPackages.x86_64-linux;
-      [
-        arduino-language-server
-        bat
-        chezmoi
-        cloudflared
-        fd
-        go-task
-        lazygit
-        neovim
-        oh-my-posh
-        shellcheck
-        shfmt
-        starship
-        stylua
+      paths = [
+        nixpkgs.legacyPackages.x86_64-linux.hello
       ];
     };
   };
