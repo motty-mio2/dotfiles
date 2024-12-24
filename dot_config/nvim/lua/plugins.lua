e490f75f6f0fe36c318df0e4af69ec8a7f77ddf0
motty <motty.mio2@gmail.com>
2024-12-24 23:19:53 +0900

    add nvim

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -56,4 +56,25 @@ return {
 		},
 		lazy = false,
 	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+	}
 }
