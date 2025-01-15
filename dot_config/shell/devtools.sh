a5cd730e5295156163c23a62c695286303db5fda
motty <motty.mio2@gmail.com>
2025-01-16 00:03:38 +0900

    add clang

--- dot_config/shell/devtools.sh
+++ dot_config/shell/devtools.sh
@@ -118,6 +118,8 @@ install-ubuntu-dev-tools() {
 	sudo apt-get install code
 	sudo apt-get install wezterm
 	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+	sudo apt install clangd clang-format clang-tidy
 }
 
 install-brew-tools() {
