7bdc3f8464fd19135948f1a11731d09b44208bfb
motty <motty.mio2@gmail.com>
2025-01-18 20:38:38 +0900

    add flatpak

--- dot_config/shell/devtools.sh
+++ dot_config/shell/devtools.sh
@@ -116,10 +116,11 @@ install-ubuntu-dev-tools() {
 	sudo apt-get update
 	sudo apt-get install golang
 	sudo apt-get install code
+	sudo apt-get install flatpak
 	sudo apt-get install wezterm
 	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-	sudo apt install clangd clang-format clang-tidy
+	sudo apt-get install clangd clang-format clang-tidy
 }
 
 install-brew-tools() {
