7e6f8969aad5667a0799f5d5dfd9f2cdbc6d603a
motty <motty.mio2@gmail.com>
2025-02-08 12:10:51 +0900

    add USERPROFILE

--- dot_config/powershell/Microsoft.PowerShell_profile.ps1
+++ dot_config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,9 @@
-$configure_folder = $Env:USERPROFILE + "\.config\powershell"
+$configure_folder = "$Env:USERPROFILE/.config/powershell"
 
-Get-ChildItem -Path ("$configure_folder\conf") -Recurse -Include *.ps1 | ForEach-Object -Process {
+Get-ChildItem -Path ("$configure_folder/conf") -Recurse -Include *.ps1 | ForEach-Object -Process {
     Invoke-Expression (". " + $_)
 }
 
-Get-ChildItem -Path ("$configure_folder\completion") -Recurse -Include *.ps1 | ForEach-Object -Process {
+Get-ChildItem -Path ("$configure_folder/completion") -Recurse -Include *.ps1 | ForEach-Object -Process {
     Invoke-Expression (". " + $_)
 }
