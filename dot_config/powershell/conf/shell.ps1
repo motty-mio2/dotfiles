4ed73c563c3f3f409bdf16b5671810dc57657b62
motty <motty.mio2@gmail.com>
2025-04-19 22:25:15 +0900

    fix sudo

--- dot_config/powershell/conf/shell.ps1
+++ dot_config/powershell/conf/shell.ps1
@@ -239,7 +239,7 @@ function Set-Better-Windows {
     # MsMpEng.exe の除外プロセス設定をクエリ
     sudo reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Defender\Exclusions\Processes" /v "MsMpEng.exe"
     # 24H2 sudo を有効化、gsudo がないとできないという矛盾はいったん置いておく
-    sudo reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Sudo" /v "Enabled" /t REG_DWORD /d "0" /f
+    sudo reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Sudo" /v "Enabled" /t REG_DWORD /d "1" /f
 
     # スタートメニュー
     # 「ときどきスタートにおすすめを表示する」をオフに設定
