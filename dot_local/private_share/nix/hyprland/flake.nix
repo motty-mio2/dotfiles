cea540100350993a206a0f9d4434ee77c2943c9f
motty <motty.mio2@gmail.com>
2025-12-11 00:21:48 +0900

    update wayland

--- dot_local/private_share/nix/hyprland/flake.nix
+++ dot_local/private_share/nix/hyprland/flake.nix
@@ -16,7 +16,7 @@
       {
         packages.hyprland = pkgs.buildEnv {
           name = "hyprland";
-          paths = with pkgs; [brightnessctl dunst hyprshot ulauncher waybar wofi
+          paths = with pkgs; [brightnessctl dunst hyprshot waybar wofi
           ];
         };
       }
