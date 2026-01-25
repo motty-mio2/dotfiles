7d0602f41368967abaa5eb717d34b19625f3ec5c
motty <motty.mio2@gmail.com>
2026-01-25 15:06:30 +0900

    update waybar

---
+++ dot_config/waybar/scripts/executable_dunst.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+ENABLED="  "
+DISABLED="  "
+
+if dunstctl is-paused | grep -q "false"; then echo "$ENABLED"; else echo "$DISABLED"; fi
--- dot_config/waybar/scripts/executable_record_timer.sh
+++ dot_config/waybar/scripts/executable_record_timer.sh
@@ -4,18 +4,17 @@
 PID=$(pgrep -x wf-recorder)
 
 if [ -n "$PID" ]; then
-    # プロセスの開始時間を取得（現在の時刻との差分を計算）
-    # ※psコマンドで経過秒数(etimes)を取得
-    SECONDS_ELAPSED=$(ps -o etimes= -p "$PID" | tr -d ' ')
-    
-    # 秒を 分:秒 (MM:SS) 形式に変換
-    MINUTES=$((SECONDS_ELAPSED / 60))
-    SECONDS=$((SECONDS_ELAPSED % 60))
-    
-    # 表示用のテキストを出力
-    printf "󰑋 %02d:%02d\n" "$MINUTES" "$SECONDS"
-else
-    # 実行中でなければ何も表示しない
-    echo ""
-fi
+	# プロセスの開始時間を取得（現在の時刻との差分を計算）
+	# ※psコマンドで経過秒数(etimes)を取得
+	SECONDS_ELAPSED=$(ps -o etimes= -p "$PID" | tr -d ' ')
 
+	# 秒を 分:秒 (MM:SS) 形式に変換
+	MINUTES=$((SECONDS_ELAPSED / 60))
+	SECONDS=$((SECONDS_ELAPSED % 60))
+
+	# 表示用のテキストを出力
+	printf "󰑋 %02d:%02d\n" "$MINUTES" "$SECONDS"
+else
+	# 実行中でなければ何も表示しない
+	echo ""
+fi
