f0cd060af81587ac7aaf0ce698aacfd2ea18383f
motty <motty.mio2@gmail.com>
2026-05-31 00:35:17 +0900

    replace with chezmoi execute-template

---
+++ dot_config/powershell/conf/packages.ps1
@@ -0,0 +1,76 @@
+if (Get-Command mise -ErrorAction SilentlyContinue) {
+    mise activate pwsh --cd $Env:USERPROFILE | Out-String | Invoke-Expression
+}
+
+function mupdate {
+    mise self-update --quiet
+    mise upgrade --bump
+    mise cache prune
+}
+
+function supdate {
+    scoop update
+    scoop update *
+    scoop cleanup *
+}
+
+function wupdate {
+    winget upgrade $args
+}
+
+
+function Install-rust-Tools {
+    $components = (chezmoi execute-template '{{.dependencies.rustup | join " " }}') -split '\s+' | Where-Object { $_ }
+    if ($components.Count -gt 0) {
+        rustup component add $components
+    }
+}
+
+function Install-uv-Tools {
+    param (
+        $UV_ENV = "$env:USERPROFILE\.local\bin",
+        $executableName = "uv.exe"
+    )
+
+    $tools = (chezmoi execute-template '{{- .dependencies.pip | join " " -}}') -split '\s+' | Where-Object { $_ }
+    foreach ($tool in $tools) {
+        & "$UV_ENV\$executableName" tool install --upgrade $tool
+    }
+
+    & "$UV_ENV\$executableName" tool install --upgrade --from git+https://github.com/motty-mio2/dixp dixp
+}
+
+function Install-Scoop-Tools {
+    $pkgs = (chezmoi execute-template '{{ range $name, $managers := .dependencies.cli -}}{{- get $managers "scoop" | printf "%s " -}}{{- end }}') -split '\s+' | Where-Object { $_ }
+    scoop install $pkgs
+}
+
+function Install-Scoop-Dev-Tools {
+    $pkgs = (chezmoi execute-template '{{ range $name, $managers := .dependencies.dev -}}{{- get $managers "scoop" | printf "%s " -}}{{- end }}') -split '\s+' | Where-Object { $_ }
+    scoop install $pkgs
+}
+
+function Install-Scoop-GUI-Tools {
+    $pkgs = (chezmoi execute-template '{{ range $name, $managers := .dependencies.desktop -}}{{- get $managers "scoop" | printf "%s " -}}{{- end }}') -split '\s+' | Where-Object { $_ }
+    scoop install 7zip geekuninstaller gsudo sysinternals $pkgs
+}
+
+function Install-Windows-Software {
+    # OpenSSH
+    winget install --id Microsoft.OpenSSH.Beta --override ADDLOCAL=Client
+
+    # PowerShell
+    winget install --id  Microsoft.PowerShell
+
+    # PowerToys
+    winget install --id XP89DCGQ3K6VLD
+
+    # Visual Studio Code
+    winget install --id Microsoft.VisualStudioCode --override "/VERYSILENT /NORESTART /MERGETASKS=!runcode,addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath"
+
+    # Install Apps
+    $wingetPkgs = (chezmoi execute-template '{{- range $name, $managers := .dependencies.desktop -}}{{- if get $managers "winget" -}}{{ get $managers "winget" | printf "%s " }}{{- end -}}{{- end -}}') -split '\s+' | Where-Object { $_ }
+    foreach ($pkg in $wingetPkgs) {
+        winget install --id $pkg
+    }
+}
